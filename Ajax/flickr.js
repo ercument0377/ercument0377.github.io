ab1ea9af40ce2eea452f803bf53fb10c18df0af4
ercument0377 <115145381+ercument0377@users.noreply.github.com>
2023-01-01 23:52:28 +0300

    Add files via upload

--- Ajax/flickr.js
+++ Ajax/flickr.js
@@ -1,12 +1,12 @@
 var url = "http://api.flickr.com/services/feeds/photos_public.gne?" +
-"format=json&jsoncallback=?&tags=waterfall,yosemite";
+"format=json&jsoncallback=?&tags=istanbul,bogaz";
 
 $.getJSON(url, function(data){
     var html = "";
     $.each(data.items, function(i, item){
         html += "<h2>" + item.title + "</h2>";	
         html += "<img src=" + item.media.m + ">";
-        html += "<p></p>";
+        html += "<p>"+item.date_taken+"</p>";
     });
 
     $("#photos").html(html);
